ca6c9fd86905b1332b0b4e6f0f00abf1c4871956
vitoria <vitoriarntrindade@gmail.com>
2024-07-15 00:17:52 -0400

    add schemas com Pydantic e testes para os  endpoints(db mockado)

--- fast_course/app.py
+++ fast_course/app.py
@@ -1,8 +1,24 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from schemas.schema_user import DbMockSchema, UserPublicSchema, UserSchema
 
 app = FastAPI()
 
+database_mock = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.post(
+    '/users/', response_model=UserPublicSchema, status_code=HTTPStatus.CREATED
+)
+def create_user(user: UserSchema):
+    user_with_id = DbMockSchema(id=len(database_mock) + 1, **user.model_dump())
+
+    database_mock.append(user_with_id)
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,25 @@ def test_root_should_returns_ok():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users',
+        json={
+            'username': 'vitoria',
+            'email': 'vitoria@gmail.com',
+            'password': '132@@JHKJ',
+        },
+    )
+
+    # Validando se o status_code está correto
+    assert response.status_code == HTTPStatus.CREATED
+    # Validando UserPublicSchema
+    assert response.json() == {
+        'username': 'vitoria',
+        'email': 'vitoria@gmail.com',
+        'id': 1
+    }
